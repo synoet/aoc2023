ec7d89ccd2cf5577b6495c2b6acd099c01e1558f
synoet <teonys@nyu.edu>
2023-12-16 21:04:53 -0500

    ADD day5 part two but it's garbage

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -41,7 +41,7 @@ fn part_one() {
                     .map(|x| x.parse::<usize>().unwrap())
                     .collect::<Vec<usize>>();
                 let range = nums[2];
-                if position <= nums[1] + range  && position >= nums[1] {
+                if position <= nums[1] + range && position >= nums[1] {
                     position = position - nums[1] + nums[0];
                     break;
                 }
@@ -51,9 +51,123 @@ fn part_one() {
     }
 
     positions.sort();
-    dbg!(positions[0]);
+}
+
+// this is fucking slow
+fn part_two() {
+    let content = read_to_string("input.txt").unwrap();
+    let lines = content.lines();
+
+    let seed_ranges = lines.clone().collect::<Vec<&str>>()[0]
+        .split(":")
+        .collect::<Vec<&str>>()[1]
+        .split(" ")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>()
+        .iter()
+        .as_slice()
+        .chunks(2)
+        .map(|chunk| {
+            let chunk = chunk.to_vec();
+            (chunk[0], chunk[1])
+        })
+        .collect::<Vec<(usize, usize)>>();
+
+    let break_pos = lines
+        .clone()
+        .enumerate()
+        .filter(|(_i, s)| s.is_empty())
+        .map(|(i, _s)| i)
+        .collect::<Vec<usize>>();
+
+    let mut groups: Vec<Vec<&str>> = vec![];
+
+    for i in 0..break_pos.len() {
+        let mut end = lines.clone().collect::<Vec<_>>().len();
+        if i < break_pos.len() - 1 {
+            end = break_pos[i + 1];
+        }
+
+        let group = &lines.clone().collect::<Vec<&str>>()[break_pos[i] + 2..end];
+        groups.push(group.to_vec());
+    }
+
+    groups.reverse();
+
+    let locations = groups[0].clone();
+    let location_ranges = locations.iter().map(|sg| {
+        let nums = sg
+            .split(" ")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
+
+        (nums[1], nums[1] + nums[2])
+    });
+
+    let remaining_groups = groups[1..].to_vec();
+
+    let mut locations_with_path = location_ranges
+        .filter_map(|location| {
+            let mut current_range = location.clone();
+            for group in remaining_groups.iter() {
+                let ranges = group
+                    .iter()
+                    .map(|sg| {
+                        sg.split(" ")
+                            .map(|x| x.parse::<usize>().unwrap())
+                            .collect::<Vec<usize>>()
+                    })
+                    .collect::<Vec<Vec<usize>>>();
+
+                if let Some(found_range) = ranges.iter().find(|r| {
+                    let range = r[2];
+                    let start = r[1];
+                    let end = start + range;
+
+                    std::cmp::max(start, location.0) <= std::cmp::min(end, location.1)
+                }) {
+                    current_range = (found_range[0], found_range[1] + found_range[2]);
+                } else {
+                    return None;
+                }
+            }
+            let maybe_seed = seed_ranges.iter().find(|(start, end)| {
+                std::cmp::max(start, &current_range.0) <= std::cmp::min(end, &current_range.1)
+            });
+
+            if let Some(seed) = maybe_seed {
+                return Some((location, seed.clone()));
+            }
+            None
+        })
+        .collect::<Vec<((usize, usize), (usize, usize))>>();
+
+    let mut ans = locations.iter().map(|l| {
+        l.split(" ")
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>()
+    })
+    .filter_map(|l| {
+        if let Some((location, seed)) = locations_with_path.iter().find(|(location, _seed)| {
+            std::cmp::max(location.1, l[1]) <= std::cmp::min(location.1, l[1] + l[2]) 
+        }) {
+            dbg!(l);
+            dbg!(seed);
+            // dbg!(seed.0 < l[1]); 
+            return Some(100);
+        }
+        None
+
+    })
+    .collect::<Vec<usize>>();
+
+
+    println!("{:?}", ans);
+
+
 }
 
 fn main() {
-    part_one();
+    part_two();
 }
